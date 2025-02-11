e5117a61daf4c7ae6a8f68e9275f6caf73d55547
Angelo Galanti <angelo.galanti@gmail.com>
2025-02-11 10:31:00 +0100

    Refactor user and photo classes for improved structure and functionality; update UML diagrams and .gitignore

--- src/es23.py
+++ src/es23.py
@@ -1,99 +1,104 @@
-from datetime import date
+from datetime import datetime
+from typing import List
+
+
 class Utente:
-    def __init__(self,profilo:tuple):
-        self._profilo = profilo
-        self._lista_seguiti = []
-        self._album_creati = []
-        self._lista_foto = []
+    def __init__(self, nome_utente: str, email: str, password: str):
+        self.nome_utente = nome_utente
+        self.email = email
+        self.password = password
+        self.immagine_profilo = None
+        self.biografia = ""
+        self.album = []
+        self.foto = []
+        self.following = []
 
-    def carica_foto(self,Foto):
-        if type(Foto) != str and type(Foto) != int and type(Foto) != float and type(Foto) != bool and type(Foto) != tuple:
-            self._lista_foto.append(Foto)
-        else:
-            raise ValueError("ERRORE CARICA_FOTO")
-        
-    def segui_utente(self,Utente):
-        if type(Utente) != str and type(Utente) != int and type(Utente) != float and type(Utente) != bool and type(Utente) != tuple:
-            self._lista_seguiti.append(Utente)
-        else:
-            raise ValueError("ERRORE SEGUI_UTENTE")
-        
+    def crea_album(self, titolo: str, descrizione: str) -> "Album":
+        nuovo_album = Album(titolo, descrizione, self)
+        self.album.append(nuovo_album)
+        return nuovo_album
+
+    def carica_foto(
+        self, titolo: str, descrizione: str, album: "Album" = None
+    ) -> "Foto":
+        nuova_foto = Foto(titolo, descrizione, self, album)
+        self.foto.append(nuova_foto)
+        if album:
+            album.aggiungi_foto(nuova_foto)
+        return nuova_foto
+
+    def segui_utente(self, utente: "Utente"):
+        if utente not in self.following:
+            self.following.append(utente)
+
+
 class Foto:
-    def __init__(self,id:str,titolo:str,descrizione:str,quando_creata:date,autore:Utente,a_che_album_appartiene):
-        self._id = id
-        self._titolo = titolo
-        self._descrizione = descrizione
-        self._quando_creata = quando_creata
-        self._autore = autore
-        self._a_che_album_appartiene = a_che_album_appartiene
+    def __init__(
+        self, titolo: str, descrizione: str, utente: Utente, album: "Album" = None
+    ):
+        # id() restituisce un identificatore unico per l'oggetto
+        self.id = id(self)  # Utilizziamo id() come identificatore unico
+        self.titolo = titolo
+        self.descrizione = descrizione
+        self.data_caricamento = datetime.now()
+        self.utente = utente
+        self.album = album
+        self.commenti = []
 
-    def metti_in_un_album(self,album):
-        album._lista_foto.append(self)
-
-    @property
-    def id(self):
-        return self._id
-    @id.setter
-    def id(self,id):
-        if type(id) == str:
-            self._id = id
-
-    @property
-    def titolo(self):
-        return self._titolo
-    @titolo.setter
-    def titolo(self,titolo):
-        if type(titolo) == str:
-            self._titolo = titolo
-    
-    @property
-    def descrizione(self):
-        return self._descrizione
-    @descrizione.setter
-    def descrizione(self,descrizione):
-        if type(descrizione) == str:
-            self._descrizione = descrizione
-
-    @property
-    def quando_creata(self):
-        return self._quando_creata
-    @quando_creata.setter
-    def quando_creata(self,quando_creata):
-        if type(quando_creata) == date:
-            self._quando_creata = quando_creata
-
-
-    @property
-    def autore(self):
-        return self._autore
-    @autore.setter
-    def autore(self,autore):
-        if type(autore) != str and type(autore) != int and type(autore) != float and type(autore) != bool and type(autore) != tuple:
-            self._autore = autore
-
-    @property
-    def a_che_album_appartiene(self):
-        return self._a_che_album_appartiene
-    @a_che_album_appartiene.setter
-    def a_che_album_appartiene(self,a_che_album_appartiene):
-        if type(a_che_album_appartiene) == str:
-            self._a_che_album_appartiene = a_che_album_appartiene
+    def aggiungi_commento(self, utente: Utente, testo: str) -> "Commento":
+        nuovo_commento = Commento(utente, self, testo)
+        self.commenti.append(nuovo_commento)
+        return nuovo_commento
 
 
 class Album:
-    def __init__(self,titolo:str,descrizione:str,autore:Utente,lista_foto:list):
-        self._titolo = titolo
-        self._descrizione = descrizione
-        self._autore = autore
-        self._lista_foto = lista_foto
+    def __init__(self, titolo: str, descrizione: str, utente: Utente):
+        self.titolo = titolo
+        self.descrizione = descrizione
+        self.utente = utente
+        self.foto: List[Foto] = []
+
+    def aggiungi_foto(self, foto: Foto):
+        if foto not in self.foto:
+            self.foto.append(foto)
+            foto.album = self
+
 
 class Commento:
-    def __init__(self,chi_lo_ha_postato:Utente,dove_e_stato_postato:Foto):
-        self._chi_lo_ha_postato = chi_lo_ha_postato
-        self._dove_e_stato_postato = dove_e_stato_postato
+    def __init__(self, utente: Utente, foto: Foto, testo: str):
+        self.utente = utente
+        self.foto = foto
+        self.testo = testo
+        self.data_creazione = datetime.now()
 
-fabio = Utente(("fabio","bila"))
-oggi = date.today
-album_foto_mare = Album("foto al mare","",fabio,[])
-foto1 = Foto(5345355,"foto di fabio al mare","che bello il mare",oggi,fabio,None)
-foto1.metti_in_un_album(album_foto_mare)
+
+# Esempio di utilizzo
+if __name__ == "__main__":
+    # Creazione utenti
+    mario = Utente("mario_rossi", "mario@email.it", "password123")
+    luigi = Utente("luigi_verdi", "luigi@email.it", "password456")
+
+    # Mario crea un album
+    album_vacanze = mario.crea_album("Vacanze 2023", "Le mie vacanze estive")
+
+    # Mario carica delle foto
+    foto1 = mario.carica_foto("Tramonto", "Un bellissimo tramonto", album_vacanze)
+    foto2 = mario.carica_foto("Spiaggia", "Giornata al mare", album_vacanze)
+
+    # Luigi segue Mario
+    luigi.segui_utente(mario)
+
+    # Luigi commenta la foto di Mario
+    foto1.aggiungi_commento(luigi, "Che bel tramonto!")
+
+    # Stampa lo stato degli oggetti
+    print("Utente Mario:", mario.nome_utente)
+    print("Utente Luigi:", luigi.nome_utente)
+    print("Album di Mario:", album_vacanze.titolo)
+    print("Foto 1 di Mario:", foto1.titolo)
+    print("Foto 2 di Mario:", foto2.titolo)
+    print("Commenti alla foto 1 di Mario:")
+    for commento in foto1.commenti:
+        print(commento.utente.nome_utente, ":", commento.testo)
+    print("Mario segue:", [utente.nome_utente for utente in mario.following])
+    print("Luigi segue:", [utente.nome_utente for utente in luigi.following])
